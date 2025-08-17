3c67292b21c6ca593eeaf1d974e86030ffa4205f
Jennynova <jeniferfernandezares@gmail.com>
2025-08-17 16:49:49 +0200

    set up results component

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,29 +1,32 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { Provider } from "react-redux";
-import { createStore, applyMiddleware } from "redux";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
-import reducers from "./reducers";
-import Home from "./components/home";
+import reducers from './reducers';
 
-import "bootstrap/dist/css/bootstrap.css";
-import "./style/main.scss";
+const createStoreWithMiddleware = applyMiddleware()(createStore);
 
-const store = createStore(reducers, applyMiddleware());
+import 'bootstrap/dist/css/bootstrap.css';
+import './style/main.scss';
+
+import Home from './components/home';
+
+import Results from './components/results';
 
 function main() {
   ReactDOM.render(
-    <Provider store={store}>
+    <Provider store={createStoreWithMiddleware(reducers)}>
       <BrowserRouter>
         <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/results" component={Home} />
+            <Route path='/' component={Home}/>
+            <Route path='/results' component={Home}/>
+            <Route path='/results' component={Results}/>
         </Switch>
       </BrowserRouter>
-    </Provider>,
-    document.querySelector(".app-wrapper")
-  );
+    </Provider>
+    , document.querySelector('.app-wrapper'));
 }
 
-document.addEventListener("DOMContentLoaded", main);
+document.addEventListener('DOMContentLoaded', main);
